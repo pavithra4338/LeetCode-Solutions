ecb2fb0f69ac091f150833e380468ec5f25cf792
pavithra4338 <pavithraists@gmail.com>
2026-08-02 12:30:23 +0530

    Time: 0 ms (100%), Space: 12.4 MB (47.77%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def stoneGame(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: bool
+        """
+        # the answer is always true
+        return True
