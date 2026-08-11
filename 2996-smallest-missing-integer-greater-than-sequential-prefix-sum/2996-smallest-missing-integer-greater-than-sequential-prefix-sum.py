b4b843b8e56e6cc4fc0bb0b3628c7071e75f6587
Pavithra Veesam <pavithraists@gmail.com>
2026-08-11 14:10:42 +0530

    Time: 0 ms (100%), Space: 12.3 MB (51.28%) - LeetHub

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def missingInteger(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        total = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1] + 1:
+                total += nums[i]
+            else:
+                break
+        while total in nums:
+            total += 1
+        return total
