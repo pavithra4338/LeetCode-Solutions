d68b71fbf20397d24ee7981a0efaf226e9b442e8
Pavithra Veesam <pavithraists@gmail.com>
2026-09-22 09:33:25 +0530

    Time: 0 ms (100%), Space: 12.8 MB (0%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.py
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.py
@@ -6,8 +6,5 @@ class Solution(object):
         """
         l=[]
         for i in range(0,len(nums)-1,2):
-            freq=nums[i]
-            value=nums[i+1]
-            for j in range(freq):
-                l.append(value)
+            l.extend([nums[i+1]]*nums[i])
         return l
