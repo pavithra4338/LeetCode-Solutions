0a84ec462fbc7a3bcd5eada12253432755b9c777
Pavithra Veesam <pavithraists@gmail.com>
2026-09-26 11:37:27 +0530

    Time: 430 ms (7.14%), Space: 58.2 MB (7.14%) - LeetHub

---
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def evaluate(self, s, knowledge):
+        """
+        :type s: str
+        :type knowledge: List[List[str]]
+        :rtype: str
+        """
+        freq={}
+        for i in range(len(knowledge)):
+            freq[knowledge[i][0]]=knowledge[i][1]
+        s=re.split(r"[()]",s)
+        print(s)
+        for i in range(1,len(s),2):
+            if s[i] in freq:
+                s[i]=freq[s[i]]
+            else:
+                s[i]="?"
+        return "".join(s)
