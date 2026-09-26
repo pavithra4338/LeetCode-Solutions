26f18f6a27a642311aed2844ac85e14f2a4df046
Pavithra Veesam <pavithraists@gmail.com>
2026-09-26 11:37:39 +0530

    Time: 68 ms (67.86%), Space: 58.1 MB (7.14%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.py
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.py
@@ -9,7 +9,6 @@ class Solution(object):
         for i in range(len(knowledge)):
             freq[knowledge[i][0]]=knowledge[i][1]
         s=re.split(r"[()]",s)
-        print(s)
         for i in range(1,len(s),2):
             if s[i] in freq:
                 s[i]=freq[s[i]]
